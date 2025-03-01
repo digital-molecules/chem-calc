5d567f61ba092982692d99a6fdd75c628b720c05
digital-molecules <thaliaarvanitidou1@hotmail.com>
2025-03-01 17:29:18 +0200

    Add files via upload
    Updated main.py to fix an issue double-character elements

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from chem_utils import compute_properties, detect_functional_groups, compute_sim
 
 st.title("Chemical Property Calculator")
 
-smiles1 = st.text_input("Enter a valid SMILES notation:").upper()
+smiles1 = st.text_input("Enter a valid SMILES notation:")
 if smiles1:
     props = compute_properties(smiles1)
     st.write("Computed Properties:", props)
@@ -12,7 +12,7 @@ if smiles1:
     funct = detect_functional_groups(smiles1)
     st.write("Functional Group(s):", funct)
 
-smiles2 = st.text_input("Enter another SMILES for similarity comparison:").upper()
+smiles2 = st.text_input("Enter another SMILES for similarity comparison:")
 if smiles1 and smiles2:
     sim = compute_similarity(smiles1, smiles2)
     st.write("Tanimoto Similarity:", sim)
