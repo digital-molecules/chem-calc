83ed3cce58a1c3aa3f813b76271bea654d0c2683
ares to cat <thaliaarvanitidou1@hotmail.com>
2025-03-06 23:27:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ if smiles1:
         st.write(props)
         qed = compute_qed(smiles1)
         st.markdown(
-        f"<h3 style='color: #4B9CD3;'>QED Drug-Likeness:</h3> <p style='font-weight: bold; style=color: #2E8B57;'>{qed}</p>", 
+        f"<h3 style='color: #4B9CD3;'>QED Drug-Likeness:</h3> <p style='font-size: 20px; color: #2E8B57;'>{qed}</p>",
         unsafe_allow_html=True
         )
 
