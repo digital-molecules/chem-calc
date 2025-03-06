458d210df1869a316c43ed71279f0cfcda7ec24b
ares to cat <thaliaarvanitidou1@hotmail.com>
2025-03-06 14:03:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import streamlit as st
 from chem_calc import compute_properties, detect_functional_groups, compute_similarity
 
 st.title("Thally's QSAR Toolkit")
-st.write("Credit to the brilliant Suliman Sharif for his public resources. You can read one of his articles [here](https://sharifsuliman.medium.com/understanding-drug-likeness-filters-with-rdkit-and-exploring-the-withdrawn-database-ebd6b8b2921e).")
+st.write("Thanks to the brilliant Suliman Sharif for his public resources and my younger brother Chris who was (mostly) available to help with the debugging.")
 
-smiles1 = st.text_input("Enter a valid SMILES notation:")
+smiles1 = st.text_input("Enter a valid SMILES notation please:")
 if smiles1:
     props = compute_properties(smiles1)
     st.write("Computed Properties:", props)
 
-#if smiles1:
-    #funct = detect_functional_groups(smiles1)
-    #st.write("Functional Group(s):", funct)
 
 smiles2 = st.text_input("Enter another SMILES for similarity comparison:")
 if smiles1 and smiles2:
     sim = compute_similarity(smiles1, smiles2)
     st.write("Tanimoto Similarity:", sim)
+
+
+
+
+
+
+    
+#if smiles1:
+    #funct = detect_functional_groups(smiles1)
+    #st.write("Functional Group(s):", funct)
