69b976b420c650ba25190f25e3d475107ad3acbc
ares to cat <thaliaarvanitidou1@hotmail.com>
2025-03-06 15:18:15 +0200

    Update main.py
    Added QED calculations on the app

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from chem_calc import compute_properties, compute_similarity
+from chem_calc import compute_properties, compute_qed, compute_similarity
 
 st.title("Thally's QSAR Toolkit")
 st.write("*Thanks to the brilliant Suliman Sharif for his public resources and my younger brother Chris who was (mostly) available to help with the debugging.*")
@@ -8,7 +8,16 @@ smiles1 = st.text_input("**Enter a valid SMILES notation please:**")
 if smiles1:
     props = compute_properties(smiles1)
     st.write("Computed Properties:", props)
-
+    
+if smiles1:
+    qed_info = qed_drug_likeness(smiles1)
+    if "Error" in qed_info:
+            st.error(qed_info["Error"])
+        else:
+            if qed_info["Passes QED Drug-Likeness"]:
+                    st.success("✅ This molecule meets the QED drug-likeness criteria! 😺")
+                else:
+                    st.warning("❌ This molecule does **NOT** meet the QED drug-likeness criteria. 😿")
 
 smiles2 = st.text_input("**Enter another SMILES for similarity comparison:**")
 if smiles1 and smiles2:
