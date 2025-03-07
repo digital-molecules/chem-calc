63d288da3a7210ba9de1ba4f0a415f16f7d4d8ce
ares to cat <thaliaarvanitidou1@hotmail.com>
2025-03-07 20:16:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ if smiles1:
         mw = compute_mw(smiles1)
         st.write("Molecular Weight:", mw)
         logp = compute_logp(smiles1)
-        st.write("logP:", logp)
+        st.write("LogP:", logp)
         tpsa = compute_tpsa(smiles1)
         st.write("TPSA:", tpsa)
         rotbond = compute_rotbond(smiles1)
